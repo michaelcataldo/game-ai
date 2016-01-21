48c39e39557872a8d8668497383dc45fb36d92b6
Michael Cataldo <info@michaelcataldo.co.uk>
2016-01-21 17:10:32 +0000

    add generic operators

--- src/game-ai.clj
+++ src/game-ai.clj
@@ -1,33 +1,22 @@
-
 ;constant grid size
 (def grid-size 10)
 
 (def world
   '#{
      ;World locations
-     (isa base location) (at base t0)
-     (isa river location) (at river t8) (at river t9) (at river t10) (at river t11)
+     ;(isa base location) (at base t0)
+     ;(isa river location) (at river t8) (at river t9) (at river t10) (at river t11)
      ;Actors
-     (isa h1 actor) (at h1 1) (holds h1 nil)
-     (isa c1 actor) (at c1 1) (holds c1 nil)
-     (isa e1 actor) (at e1 1) (holds e1 nil)
+     (isa h1 actor) (at h1 t1) (holds h1 :nil) (handles :nil h1)
+     (isa c1 actor) (at c1 t1) (handles prepared c1)
+     ;(isa e1 actor) (at e1 base)
      ;Resources
-     (isa r1 resource) (at r1 t5) (prepared r1)
-     (isa r2 resource) (at r2 t6)
-     (isa r3 resource) (at r3 t7)
-     })
+     (isa r1 resource) (unprepared r1) (at r1 t2)
+     ;     (isa r2 resource) (at r2 t2) (prepared r2)
+     ;     (isa r3 resource) (at r3 t2) (prepared r3)
 
-(defn calc-adj [pos]
-  (list
-    (- pos 1)
-    (+ pos 1)
-    (+ pos grid-size)
-    (- pos grid-size)
-    (+ (- pos grid-size) 1)
-    (- (- pos grid-size) 1)
-    (+ (+ pos grid-size) 1)
-    (- (+ pos grid-size) 1)
-    )
+     ;(blocked t10)
+     }
   )
 
 (defn make-tile-id [c grid]
@@ -42,8 +31,8 @@
   (let [t (make-tile-id `(~x ~y) grid)]
     (concat
       (list
-        `(~'isa ~t ~'tile)
-        `(~'at (~x ~y) ~t))
+        `(~'isa ~t ~'tile))
+      ;`(~'at (~x ~y) ~t))
       tiles)
     )
   )
@@ -100,13 +89,6 @@
     )
   )
 
-(defn update-path
-  [current newp]
-  {:state (:state newp),
-   :cmds (concat (:cmds current) (:cmd newp)),
-   :txt (concat (:txt current) (:txt newp))
-   })
-
 ;Get adjacent tiles of the passed
 (defn get-valid-moves [tile]
   (let [patterns (map (fn [x] `(~'isa ~x ~'tile)) (calc-adj tile))]
@@ -114,145 +96,122 @@
     )
   )
 
+(def generic-ops
+  '{moveto
+    {:name moveto
+     :achieves (at ?actor ?t2)
+     :when ((isa ?actor actor) (isa ?t1 tile) (isa ?t2 tile) (at ?actor ?t1) (:guard (not= (? t1) (? t2))) (:not (blocked ?t2)))
+     ;:post (())
+     :pre ((at ?actor ?t1))
+     :del ((at ?actor ?t1))
+     :add ((at ?actor ?t2))
+     ;:cmd (())
+     :txt (?actor moves from ?t1 to ?t2)
+     }
+    pickup
+    {:name pickup
+     :achieves (holds ?actor ?r)
+     :when ((isa ?actor actor)(at ?r ?t) (handles ?rs ?actor) (:not (handles :nil ?actor)))
+     :post ((holds ?actor :nil) (at ?actor ?t))
+     :pre ((isa ?actor actor) (isa ?r resource) (at ?r ?t)  (?rs ?r))
+     :del ((at ?r ?t) (holds ?actor :nil))
+     :add ((holds ?actor ?r))
+     ;:cmd (())
+     :txt (?actor picks up ?r from ?t)
+     }
+    drop
+    {:name drop
+     :achieves (at ?r ?t)
+     :when ((isa ?actor actor))
+     :post ((holds ?actor ?r) (at ?actor ?t))
+     :pre ( (isa ?r resource)(holds ?actor ?r) (at ?actor ?t)) ;; currently can have mutliple things on one tile
+     :del ((holds ?actor ?r))
+     :add ((at ?r ?t) (holds ?actor :nil))
+     ;:cmd (())
+     :txt (?actor drops ?r on ?t)
+     }
+
+    prepare
+    {:name prepare
+     :achieves (prepared ?r)
+     :when ((at ?r ?t))
+     :post ((at ?actor ?t))
+     :pre ((isa ?actor actor) (unprepared ?r))
+     :del ((unprepared ?r))
+     :add ((prepared ?r))
+     :cmd ()
+     :txt (?actor prepares ?r at ?t)
+     }
+
+
+    }
+  )
+
 (def harvester-ops
-  '{search
-    {:name search
-     :achieves (at ?actor ?tile2) ;tile1 = currentTile tile2 = destinationTile
-     :when ((at ?actor ?tile1) (:guard (not= (? tile1) (? tile2))))
-     :post ()
-     :pre ((contains? (get-valid-moves ?tile1) `(~'isa ~?tile2 ~'tile)))
-     :del ((at ?actor ?tile1))
-     :add ((at ?actor ?tile2))
-     :cmd ()
-     :txt (?actor moves from ?tile1 to ?tile2)
-     }
-    prepare
+  '{prepare
     {:name prepare
-     :achieves (at ?res ?restile)
-     :when ((next-to ?actor ?rawsource))
-     :post ()
-     :pre ((next-to ?actor ?rawsource))
-     :del ((at ?rawsource ?restile))
-     :add ((at ?res ?restile))
+     :achieves (prepared ?r)
+     :when ((at ?r ?t))
+     :post ((at ?actor ?t))
+     :pre ((isa ?actor actor) (unprepared ?r))
+     :del ((unprepared ?r))
+     :add ((prepared ?r))
      :cmd ()
-     :txt (?actor prepares a ?res at ?restile)
-     }
-    notify
-    {:name notify
-     :achieves (on ?x ?y)
-     :when ((at ?x ?sx) (at ?y ?sy) (:guard (not= (? sx) (? sy))))
-     :post ((protected ?sx) (protected ?sy)
-             (cleartop ?x)
-             (cleartop ?y)
-             (hand empty))
-     :pre ()
-     :del ((at ?x ?sx)
-            (cleartop ?y)
-            (protected ?sx)
-            (protected ?sy))
-     :add ((at ?x ?sy)
-            (on ?x ?y))
-     :cmd ((pick-from ?sx)
-            (drop-at ?sy))
-     :txt (put ?x on ?y)
-
+     :txt (?actor prepares ?r at ?t)
      }
     })
 
-(def transporter-ops
-  '{load
-    {:name load
-     :achieves (loaded ?res ?transporter) ;tile1 = currentTile tile2 = destinationTile
-     :when ((next-to ?res ?transporter))
-     :post ()
-     :pre ((next-to ?res ?transporter)
-            (empty ?transporter))
-     :del ((next-to ?res ?transporter)
-            (empty ?transporter))
-     :add ((loaded ?res ?transporter))
-     :cmd ()
-     :txt (?transporter loads ?res)
-     }
-    prepare
-    {:name prepare
-     :achieves (at ?res ?restile)
-     :when ((next-to ?actor ?rawsource))
-     :post ()
-     :pre ((next-to ?actor ?rawsource))
-     :del ((at ?rawsource ?restile))
-     :add ((at ?res ?restile))
-     :cmd ()
-     :txt (?actor prepares a ?res at ?restile)
-     }
-    notify
-    {:name notify
-     :achieves (on ?x ?y)
-     :when ((at ?x ?sx) (at ?y ?sy) (:guard (not= (? sx) (? sy))))
-     :post ((protected ?sx) (protected ?sy)
-             (cleartop ?x)
-             (cleartop ?y)
-             (hand empty))
-     :pre ()
-     :del ((at ?x ?sx)
-            (cleartop ?y)
-            (protected ?sx)
-            (protected ?sy))
-     :add ((at ?x ?sy)
-            (on ?x ?y))
-     :cmd ((pick-from ?sx)
-            (drop-at ?sy))
-     :txt (put ?x on ?y)
+(def collector-ops
+  '{})
 
-     }
-    })
+    (def engineer-ops
+      '{build
+        {:name build
+         :achieves (on bridge ?tile) ;tile1 = currentTile tile2 = destinationTile
+         :when ((on ?actor ?tile)
+                 (has ?actor ?resource))
+         :post ()
+         :pre ((on ?actor ?tile)
+                (has ?actor ?resource)
+                (connects ?tile ?tile1)
+                (on water ?tile1)
+                )
+         :del ((has ?actor ?resource)
+                (on water ?tile1))
+         :add ((on bridge ?tile))
+         :cmd ()
+         :txt (?actor builds a bridge on ?tile)
+         }
+        prepare
+        {:name prepare
+         :achieves (at ?res ?restile)
+         :when ((next-to ?actor ?rawsource))
+         :post ()
+         :pre ((next-to ?actor ?rawsource))
+         :del ((at ?rawsource ?restile))
+         :add ((at ?res ?restile))
+         :cmd ()
+         :txt (?actor prepares a ?res at ?restile)
+         }
+        notify
+        {:name notify
+         :achieves (on ?x ?y)
+         :when ((at ?x ?sx) (at ?y ?sy) (:guard (not= (? sx) (? sy))))
+         :post ((protected ?sx) (protected ?sy)
+                 (cleartop ?x)
+                 (cleartop ?y)
+                 (hand empty))
+         :pre ()
+         :del ((at ?x ?sx)
+                (cleartop ?y)
+                (protected ?sx)
+                (protected ?sy))
+         :add ((at ?x ?sy)
+                (on ?x ?y))
+         :cmd ((pick-from ?sx)
+                (drop-at ?sy))
+         :txt (put ?x on ?y)
 
-(def engineer-ops
-  '{build
-    {:name build
-     :achieves (on bridge ?tile) ;tile1 = currentTile tile2 = destinationTile
-     :when ((on ?actor ?tile)
-             (has ?actor ?resource))
-     :post ()
-     :pre ((on ?actor ?tile)
-            (has ?actor ?resource)
-            (connects ?tile ?tile1)
-            (on water ?tile1)
-            )
-     :del ((has ?actor ?resource)
-            (on water ?tile1))
-     :add ((on bridge ?tile))
-     :cmd ()
-     :txt (?actor builds a bridge on ?tile)
-     }
-    prepare
-    {:name prepare
-     :achieves (at ?res ?restile)
-     :when ((next-to ?actor ?rawsource))
-     :post ()
-     :pre ((next-to ?actor ?rawsource))
-     :del ((at ?rawsource ?restile))
-     :add ((at ?res ?restile))
-     :cmd ()
-     :txt (?actor prepares a ?res at ?restile)
-     }
-    notify
-    {:name notify
-     :achieves (on ?x ?y)
-     :when ((at ?x ?sx) (at ?y ?sy) (:guard (not= (? sx) (? sy))))
-     :post ((protected ?sx) (protected ?sy)
-             (cleartop ?x)
-             (cleartop ?y)
-             (hand empty))
-     :pre ()
-     :del ((at ?x ?sx)
-            (cleartop ?y)
-            (protected ?sx)
-            (protected ?sy))
-     :add ((at ?x ?sy)
-            (on ?x ?y))
-     :cmd ((pick-from ?sx)
-            (drop-at ?sy))
-     :txt (put ?x on ?y)
+         }
+        })
 
-     }
-    })
