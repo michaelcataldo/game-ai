4cc5aa9c157e870a7fb06af8aedfc327ef74a551
Michael Cataldo <info@michaelcataldo.co.uk>
2016-01-26 12:52:32 +0000

    Aced basic operator functionality.

--- src/game-ai.clj
+++ src/game-ai.clj
@@ -7,16 +7,44 @@
      ;(isa base location) (at base t0)
      ;(isa river location) (at river t8) (at river t9) (at river t10) (at river t11)
      ;Actors
-     (isa h1 actor) (at h1 t1) (holds h1 :nil) (handles :nil h1)
-     (isa c1 actor) (at c1 t1) (handles prepared c1)
+     (isa h1 actor) (at h1 t1) (holds h1 :nil) (handles h1 unprepared)
+     (isa c1 actor) (at c1 t2) (holds c1 :nil) (handles c1 prepared)
+     (isa e1 actor) (at e1 t3) (holds e1 :nil) (handles e1 stored)
+
+     (on river t8)
+
+
      ;(isa e1 actor) (at e1 base)
      ;Resources
-     (isa r1 resource) (unprepared r1) (at r1 t2)
+     (isa r1 resource) (unprepared r1) (at r1 t5)
      ;     (isa r2 resource) (at r2 t2) (prepared r2)
      ;     (isa r3 resource) (at r3 t2) (prepared r3)
 
      ;(blocked t10)
      }
+  )
+
+(defn coord-adjs [c gs]
+  (remove
+    #(or
+       (>= (first %) gs)
+       (>= (second %) gs)
+       (< (first %) 0)
+       (< (second %) 0))
+    (let [x (first c)
+          y (second c)]
+      (list
+        `(~(inc x) ~y)
+        `(~(dec x) ~y)
+        `(~x ~(inc y))
+        `(~x ~(dec y))
+        `(~(inc x) ~(inc y))
+        `(~(dec x) ~(dec y))
+        `(~(inc x) ~(dec y))
+        `(~(dec x) ~(inc y))
+        )
+      )
+    )
   )
 
 (defn make-tile-id [c grid]
@@ -66,35 +94,12 @@
     )
   )
 
-(defn coord-adjs [c gs]
-  (remove
-    #(or
-       (>= (first %) gs)
-       (>= (second %) gs)
-       (< (first %) 0)
-       (< (second %) 0))
-    (let [x (first c)
-          y (second c)]
-      (list
-        `(~(inc x) ~y)
-        `(~(dec x) ~y)
-        `(~x ~(inc y))
-        `(~x ~(dec y))
-        `(~(inc x) ~(inc y))
-        `(~(dec x) ~(dec y))
-        `(~(inc x) ~(dec y))
-        `(~(dec x) ~(inc y))
-        )
-      )
-    )
-  )
-
-;Get adjacent tiles of the passed
-(defn get-valid-moves [tile]
-  (let [patterns (map (fn [x] `(~'isa ~x ~'tile)) (calc-adj tile))]
-    (set (intersection (set patterns) (set world)))
-    )
-  )
+;;Get adjacent tiles of the passed
+;(defn get-valid-moves [tile]
+;  (let [patterns (map (fn [x] `(~'isa ~x ~'tile)) (calc-adj tile))]
+;    (set (intersection (set patterns) (set world)))
+;    )
+;  )
 
 (def generic-ops
   '{moveto
@@ -111,9 +116,9 @@
     pickup
     {:name pickup
      :achieves (holds ?actor ?r)
-     :when ((isa ?actor actor)(at ?r ?t) (handles ?rs ?actor) (:not (handles :nil ?actor)))
-     :post ((holds ?actor :nil) (at ?actor ?t))
-     :pre ((isa ?actor actor) (isa ?r resource) (at ?r ?t)  (?rs ?r))
+     :when ((at ?r ?t)  (handles ?actor ?rs) (:not(unprepared ?r)))
+     :post ((?rs ?r) (at ?actor ?t))
+     :pre ((handles ?actor ?rs))
      :del ((at ?r ?t) (holds ?actor :nil))
      :add ((holds ?actor ?r))
      ;:cmd (())
@@ -122,19 +127,18 @@
     drop
     {:name drop
      :achieves (at ?r ?t)
-     :when ((isa ?actor actor))
+     :when ((handles ?actor prepared)) ;;restricted to using prepared instead of ?rs multiple goals should solve
      :post ((holds ?actor ?r) (at ?actor ?t))
-     :pre ( (isa ?r resource)(holds ?actor ?r) (at ?actor ?t)) ;; currently can have mutliple things on one tile
-     :del ((holds ?actor ?r))
+     :pre  ((isa ?r resource) (holds ?actor ?r) (at ?actor ?t)) ;; currently can have mutliple things on one tile
+     :del ((holds ?actor ?r) )
      :add ((at ?r ?t) (holds ?actor :nil))
      ;:cmd (())
      :txt (?actor drops ?r on ?t)
      }
-
     prepare
     {:name prepare
      :achieves (prepared ?r)
-     :when ((at ?r ?t))
+     :when ((at ?r ?t) (handles ?actor unprepared))
      :post ((at ?actor ?t))
      :pre ((isa ?actor actor) (unprepared ?r))
      :del ((unprepared ?r))
@@ -142,8 +146,28 @@
      :cmd ()
      :txt (?actor prepares ?r at ?t)
      }
-
-
+    store
+    {:name store
+     :achieves (stored ?r)
+     :when ((handles ?actor prepared))
+     :post ((prepared ?r) (holds ?actor ?r) (at ?actor t1)) ;;Order of post conditions threw raise condition
+     :pre ((isa ?actor actor) (prepared ?r))
+     :del ((prepared ?r) (holds ?actor ?r))
+     :add ((stored ?r) (holds ?actor :nil) (at ?r t1))
+     :cmd ()
+     :txt (?actor stores ?r at t1)
+     }
+    build
+    {:name build
+     :achieves (on bridge ?t)
+     :when ((isa ?r resource) (handles ?actor stored) (on river ?t))
+     :post ((stored ?r)(holds ?actor ?r)(at ?actor ?t))
+     :pre  ((isa ?r resource) (holds ?actor ?r) (at ?actor ?t))
+     :del ((holds ?actor ?r) (on river ?t) (stored ?r) (isa ?r resource))
+     :add ((on bridge ?t) (holds ?actor :nil))
+     ;:cmd (())
+     :txt (?actor builds a bridge at ?t)
+     }
     }
   )
 
